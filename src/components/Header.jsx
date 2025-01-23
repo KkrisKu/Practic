7671c861310a2b0767eda4d1b40c28dac2514a67
KkrisKu <kupyn.kristy@gmail.com>
2025-01-23 17:19:31 +0200

    Second Try

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
         <>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home" style={{ fontSize: "24px", fontWeight: "bold" }}>
+                    <Navbar.Brand href="#home" style={{ fontSize: "30px", fontWeight: "bold" }}>
                         ONLINE COURSES
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -17,6 +17,10 @@ export default function Header() {
                             <Nav.Link href="#courses">COURSES</Nav.Link>
                             <Nav.Link href="#about-us">ABOUT US</Nav.Link>
                             <Nav.Link href="#contacts">CONTACTS</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#login">LOGIN</Nav.Link>
+                            <Nav.Link href="#register">REGISTER</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
